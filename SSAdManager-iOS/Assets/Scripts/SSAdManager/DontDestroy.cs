f2064cc52e157e80f5c27bb92b0f1e57749c2a0b
Fredd Eyles <freddeyles@hotmail.com>
2013-06-11 16:42:56 -0400

    Fixed Tapjoy bug, Prefabs Do not get Destroyed When changing scenes

---
+++ SSAdManager-iOS/Assets/Scripts/SSAdManager/DontDestroy.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class DontDestroy : MonoBehaviour {
+	
+	void Awake()
+	{
+		DontDestroyOnLoad(this.gameObject);	
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
